fdf87f74c1624677c1de6960e1de889a28dddc38
Jean Pavan <jnopavan@gmail.com>
2021-05-04 21:20:22 -0300

    update test

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h1>Home 3</h1>
+            <h1>Home</h1>
             <Contador />
         </div>
     )
