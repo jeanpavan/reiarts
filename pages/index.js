246fc3feae3b09dafa136a73733ceac97ab49471
Jean Pavan <jnopavan@gmail.com>
2021-05-05 14:03:24 -0300

    Adição de mais funções

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,39 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Contador />
+            <Counter />
+            <CapsLock1 text="Jeito 1: Me deixe em Caps Lock por favor" />
+            <CapsLock2>Jeito 2: Me deixe em Caps Lock por favor</CapsLock2>
         </div>
     )
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1);
+function Counter() {
+    const [index, setCounter] = useState(1);
 
-    function adicionarContador() {
-        setContador(contador + 1);
+    function addCounter() {
+        console.log('Adicionou');
+        setCounter(index + 1);
     }
 
     return (
         <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <div>{index}</div>
+            <button onClick={addCounter}>Adicionar</button>
         </div>
     )
+}
+
+function CapsLock1(props) {
+    const originalText = props.text
+    const capsLockText = originalText.toUpperCase();
+    return <div>{capsLockText}</div>
+}
+
+function CapsLock2(props) {
+    const originalText = props.children;
+    const capsLockText = originalText.toUpperCase();
+    return <div>{capsLockText}</div>
 }
 
 export default Home
